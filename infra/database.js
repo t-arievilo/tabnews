8f7f912133d100549b4957eb0626b2ded23f6f39
Thiago Oliveira <132385595+t-arievilo@users.noreply.github.com>
2026-03-13 20:03:15 +0000

    makes databa.js client.connect() more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,21 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("credenciais do postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     const res = await client.query(queryObject);
     return res;
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
