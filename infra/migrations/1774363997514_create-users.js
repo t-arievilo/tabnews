8c9b6ea9d8a6f24568f684ee015678ffdd1cfca1
Thiago Oliveira <132385595+t-arievilo@users.noreply.github.com>
2026-03-24 16:02:01 +0000

    feat: add migration to create users table

---
+++ infra/migrations/1774363997514_create-users.js
@@ -0,0 +1,39 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    // For reference, GitHub limits usernames to 39 chareacters
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+    // Why 254 in lenght? https://stackoverflow.com/a/1199238
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+    // Why 60 in lenght? http://www.npmjs.com/package/bcrypt#hash-info
+    password: {
+      type: "varchar(72)",
+      notNull: true,
+    },
+    // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestampz/
+    create_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc',now())"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc',now())"),
+    },
+  });
+};
+
+exports.down = false;
