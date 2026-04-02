e30c2da9865694b6155068965fd8e0cec9c68a1d
Thiago Oliveira <thiago.math@hotmail.com>
2026-04-02 13:35:27 -0300

    feat: implement method delete to del user cookies(logout)

--- infra/controller.js
+++ infra/controller.js
@@ -40,6 +40,16 @@ async function setSessionCookie(sessionToken, response) {
 
   response.setHeader("Set-Cookie", setCookie);
 }
+async function clearSessionCookie(response) {
+  const setCookie = cookie.serialize("session_id", "invalid", {
+    path: "/",
+    maxAge: -1,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  response.setHeader("Set-Cookie", setCookie);
+}
 
 const controller = {
   errorHandlers: {
@@ -47,6 +57,7 @@ const controller = {
     onError: onErrorHandler,
   },
   setSessionCookie,
+  clearSessionCookie,
 };
 
 export default controller;
---
+++ tests/integration/api/v1/sessions/delete.test.js
@@ -0,0 +1,126 @@
+import session from "models/session";
+import orchestrator from "tests/orchestrator.js";
+import { version as uuidVersion } from "uuid";
+import setCookieParser from "set-cookie-parser";
+import user from "pages/api/v1/user";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+describe("DELETE /api/v1/sessions", () => {
+  describe("Default user", () => {
+    test("With nonexistent session", async () => {
+      const nonexistentToken =
+        "dajgh23y829idj89h28nd828h8dh8ih2ukh328u329j20jdj82dh";
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "DELETE",
+        headers: {
+          Cookie: `session_id=${nonexistentToken}`,
+        },
+      });
+      expect(response.status).toEqual(401);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Usuário não possui sessão ativa",
+        action: "Verifique se este usuário está logado e tente novamente",
+        status_code: 401,
+      });
+    });
+    test("With expired session", async () => {
+      jest.useFakeTimers({
+        now: new Date(Date.now() - session.EXPIRATION_IN_MILLISECONDS),
+      });
+
+      const createdUser = await orchestrator.createUser();
+      const sessionObject = await orchestrator.createSession(createdUser.id);
+
+      jest.useRealTimers();
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "DELETE",
+        headers: {
+          Cookie: `session_id=${sessionObject.token}`,
+        },
+      });
+      expect(response.status).toEqual(401);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Usuário não possui sessão ativa",
+        action: "Verifique se este usuário está logado e tente novamente",
+        status_code: 401,
+      });
+    });
+    test("With valid session", async () => {
+      const createdUser = await orchestrator.createUser();
+      const sessionObject = await orchestrator.createSession(createdUser.id);
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "DELETE",
+        headers: {
+          Cookie: `session_id=${sessionObject.token}`,
+        },
+      });
+      expect(response.status).toEqual(200);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        id: sessionObject.id,
+        token: sessionObject.token,
+        user_id: sessionObject.user_id,
+        expires_at: responseBody.expires_at,
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.expires_at)).not.toBeNaN();
+
+      expect(
+        responseBody.expires_at < sessionObject.expires_at.toISOString(),
+      ).toEqual(true);
+      expect(
+        responseBody.updated_at > sessionObject.updated_at.toISOString(),
+      ).toEqual(true);
+
+      // Set-Cookie assertions
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: "invalid",
+        maxAge: -1,
+        path: "/",
+        httpOnly: true,
+      });
+
+      //Double check assertions
+      const doubleCheckResponse = await fetch(
+        "http://localhost:3000/api/v1/user",
+        {
+          headers: {
+            Cookie: `session_id=${sessionObject.token}`,
+          },
+        },
+      );
+
+      expect(doubleCheckResponse.status).toEqual(401);
+
+      const doubleCheckResponseBody = await doubleCheckResponse.json();
+      expect(doubleCheckResponseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Usuário não possui sessão ativa",
+        action: "Verifique se este usuário está logado e tente novamente",
+        status_code: 401,
+      });
+    });
+  });
+});
